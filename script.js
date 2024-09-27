d55af99083f664f49a5e0bc77acb19a2a9970192
AlanFS <dev.alanfs@gmail.com>
2024-09-26 23:21:00 -0300

    retirada de cláusulas sem uso

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const btnTopo = document.querySelector(".btn-topo");
     function handleBtnTopo() {
       window.addEventListener("scroll", () => {
-        console.log(window.scrollY);
         window.scrollY > 100
           ? (btnTopo.style.display = "flex")
           : (btnTopo.style.display = "none");
